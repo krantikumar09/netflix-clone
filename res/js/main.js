dbe3b9b1a7e338d62534a39b6cb6c4a46162005e
Krantikumar Mathapati <krantikumar3446@gmail.com>
2023-10-08 16:17:23 +0530

    Implemented password show and hide

--- res/js/main.js
+++ res/js/main.js
@@ -1,6 +1,11 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+const emailError = document.getElementById("email-error");
+const passError = document.getElementById("pass-error");
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
@@ -12,4 +17,41 @@ for (i = 0; i < acc.length; i++) {
       panel.style.height = "auto";
     }
   });
+}
+
+email.addEventListener("keyup", () => {
+  if (email.value.length == 0) {
+    emailError.innerText = "Please enter a valid email or phone number.";
+    return false;
+  }
+
+  if (email.value.length > 4) {
+    emailError.innerText = "";
+    return true;
+  }
+
+  emailError.innerText = "";
+  return true;
+});
+
+pass.addEventListener("keyup", () => {
+  if (pass.value.length < 4) {
+    passError.innerText =
+      "Your password must contain between 4 and 60 characters.";
+    return false;
+  }
+  passError.innerText = "";
+  return true;
+});
+
+function handlePass() {
+  const showpass = document.getElementById("showPass");
+
+  if (pass.type == "password") {
+    pass.type = "text";
+    showpass.innerText = "HIDE";
+  } else {
+    pass.type = "password";
+    showpass.innerText = "SHOW";
+  }
 }
